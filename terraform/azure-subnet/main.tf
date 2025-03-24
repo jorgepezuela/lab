9808231befa97bb4909ddd2ac1b5f039a225d0d7
jpezuela <jorge.pezuela@hotmail.com>
2025-03-24 12:27:42 +0100

    azure-subnet tf module added

---
+++ terraform/azure-subnet/main.tf
@@ -0,0 +1,81 @@
+#-------------------------------
+# Local Declarations
+#-------------------------------
+locals {
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp[*].name, azurerm_resource_group.rg[*].name, [""]), 0)
+}
+
+#---------------------------------------------------------
+# Resource Group Creation or selection - Default is "true"
+#---------------------------------------------------------
+data "azurerm_resource_group" "rgrp" {
+  count = var.create_resource_group == false ? 1 : 0
+  name  = var.resource_group_name
+}
+
+resource "azurerm_resource_group" "rg" {
+  #ts:skip=AC_AZURE_0389 RSG lock should be skipped for now.
+  count    = var.create_resource_group ? 1 : 0
+  name     = lower(var.resource_group_name)
+  location = var.location
+  tags     = merge({ "ResourceName" = format("%s", var.resource_group_name) }, var.tags, )
+}
+
+data "azurerm_virtual_network" "main" {
+  name                = var.virtual_network_name
+  resource_group_name = var.vnet_resource_group_name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = lower(var.name)
+  resource_group_name  = var.vnet_resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.main.name
+  address_prefixes     = var.address_prefixes
+
+  service_endpoints = var.service_endpoints
+
+  dynamic "delegation" {
+    for_each = var.subnet_delegation
+    content {
+      name = delegation.key
+      dynamic "service_delegation" {
+        for_each = toset(delegation.value)
+        content {
+          name    = service_delegation.value.name
+          actions = service_delegation.value.actions
+        }
+      }
+    }
+  }
+
+  private_link_service_network_policies_enabled = var.private_link_policies
+  private_endpoint_network_policies             = var.private_endpoint_network_policies
+
+}
+
+data "azurerm_network_security_group" "main" {
+  count               = var.network_security_group_name == null ? 0 : 1
+  name                = var.network_security_group_name
+  resource_group_name = local.resource_group_name
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  count                     = var.network_security_group_name == null ? 0 : 1
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = data.azurerm_network_security_group.main[count.index].id
+}
+
+data "azurerm_route_table" "main" {
+  count               = var.route_table_name == null ? 0 : 1
+  name                = var.route_table_name
+  resource_group_name = local.resource_group_name
+}
+
+resource "azurerm_subnet_route_table_association" "main" {
+  count          = var.route_table_name == null ? 0 : 1
+  subnet_id      = azurerm_subnet.main.id
+  route_table_id = data.azurerm_route_table.main[count.index].id
+  lifecycle {
+    ignore_changes = [route_table_id]
+  }
+}
---
+++ terraform/azure-subnet/variables.tf
@@ -0,0 +1,86 @@
+variable "name" {
+  description = "Name of Azure Subnet"
+  type        = string
+}
+
+variable "create_resource_group" {
+  description = "Whether to create resource group and use it for all network resources"
+  default     = true
+  type        = bool
+}
+
+variable "resource_group_name" {
+  description = "Name of Azure Resource Group for the subnet"
+  type        = string
+}
+
+variable "vnet_resource_group_name" {
+  description = "Name of Azure Resource Group for the vnet"
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default     = "westeurope"
+  type        = string
+}
+
+variable "virtual_network_name" {
+  description = "Name of Azure Virtual Network for the subnet"
+  type        = string
+}
+
+variable "address_prefixes" {
+  description = "Address prefixes of the subnet"
+  type        = list(string)
+}
+
+variable "service_endpoints" {
+  description = "List of Azure Service Endpoints to associate with the subnet"
+  type        = list(string)
+  default     = []
+}
+
+variable "subnet_delegation" {
+  description = <<EOD
+  "Configuration of the subnet delegation"
+  object({
+    name = object({
+      name = string,
+      actions = list(string)
+    })
+  })
+  EOD
+  type        = map(list(any))
+  default     = {}
+}
+
+variable "private_link_policies" {
+  description = "Enable or disable network policies for private link on the subnet"
+  type        = bool
+  default     = true
+}
+
+variable "private_endpoint_network_policies" {
+  description = "(Optional) Enable or Disable network policies for the private endpoint on the subnet. Possible values are Disabled, Enabled, NetworkSecurityGroupEnabled and RouteTableEnabled. Defaults to Disabled"
+  type        = string
+  default     = "Disabled"
+}
+
+variable "network_security_group_name" {
+  description = "Name of the network security group to associate with the subnet"
+  type        = string
+  default     = null
+}
+
+variable "route_table_name" {
+  description = "Name of the route table to associate with the subnet"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources created."
+  type        = map(string)
+  default     = {}
+}
